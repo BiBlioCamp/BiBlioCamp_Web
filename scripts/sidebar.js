a98f55f774938f67cbd34dc0fbf7fa4925a5789a
B1nair3 <jgamec2019@gmail.com>
2024-05-24 09:47:08 -0300

    ajuda finalizado? sidebar atual será excluida e refeita totalmente do zero

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,7 +1,6 @@
 const btn = document.querySelector('.menu');
 const text = document.querySelector('.logo');
 const list = document.querySelector('.list');
-const btnClose = document.querySelector('.close');
 const logo = document.querySelector('.logoLink');
 const aloc = document.querySelector('.alocacoes');
 const name = document.querySelector('.name');
@@ -35,23 +34,6 @@ btn.addEventListener('click',
             menu.style.width = '13rem';
             aloc.style.opacity = '1';
             name.style.opacity = '1';
-        }
-    }
-)
-btnClose.addEventListener('click', 
-    function () {
-        btn.classList.toggle('active');
-        if (btn.classList.contains('active')) {
-            text.style.display = 'none';
-            btn.style.display = 'none';
-            profile.style.display = 'none';
-            list.style.display = 'none';
-        }
-        else {
-            text.style.display = 'block';
-            btn.style.display = 'block';
-            profile.style.display = 'block';
-            list.style.display = 'block';
         }
     }
 )
