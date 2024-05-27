774cfa0d51a8d50a89b83abfd26d3906fed6b1bc
B1nair3 <jgamec2019@gmail.com>
2024-05-27 11:46:13 -0300

    Sidebar refeita e aplicada para todas as paginas

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,39 +1,14 @@
-const btn = document.querySelector('.menu');
-const text = document.querySelector('.logo');
-const list = document.querySelector('.list');
-const logo = document.querySelector('.logoLink');
-const aloc = document.querySelector('.alocacoes');
-const name = document.querySelector('.name');
-const pfp = document.querySelector('.pfp');
-const ajuda = document.querySelector('.ajuda');
-const contato = document.querySelector('.contato');
-const acervo = document.querySelector('.acervo');
-const home = document.querySelector('.home');
-const menu = document.querySelector('.left-menu');
-const settings = document.getElementById('setting');
-
-btn.addEventListener('click', 
-    function () {
-        btn.classList.toggle('active');
-        if (btn.classList.contains('active')) {
-            menu.style.width = '4.6rem';
-            home.style.opacity = '0';
-            contato.style.opacity = '0';
-            ajuda.style.opacity = '0';
-            acervo.style.opacity = '0';
-            logo.style.opacity = '0';
-            name.style.opacity = '0';
-            aloc.style.opacity = '0';
-        }
-        else {
-            logo.style.opacity = '1';
-            home.style.opacity = '1';
-            contato.style.opacity = '1';
-            ajuda.style.opacity = '1';
-            acervo.style.opacity = '1';
-            menu.style.width = '13rem';
-            aloc.style.opacity = '1';
-            name.style.opacity = '1';
-        }
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+      arrow[i].addEventListener("click", (e)=>{
+     let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+     arrowParent.classList.toggle("showMenu");
+      });
     }
-)
+    
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", ()=>{
+      sidebar.classList.toggle("close");
+    });
